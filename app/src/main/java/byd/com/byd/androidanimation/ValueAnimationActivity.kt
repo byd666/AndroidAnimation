6666651acd7893165087e1b729ab2e4bf1bdb261
byd <sdkjdxbyd@163.com>
2018-05-10 17:10:08 +0800

    完结

--- app/src/main/java/byd/com/byd/androidanimation/ValueAnimationActivity.kt
+++ app/src/main/java/byd/com/byd/androidanimation/ValueAnimationActivity.kt
@@ -9,9 +9,6 @@ import android.view.View
 import android.widget.Button
 
 class ValueAnimationActivity : AppCompatActivity(), View.OnClickListener{
-
-
-
     private var view: View ?=null
     private var btn1: Button ?=null
     private var btn2: Button ?=null
